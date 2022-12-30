a7174bb31f4890f77721eb090b5e40b5a2e49159
novanAfnd <novanafandi01@gmail.com>
2022-12-30 21:46:53 +0700

    Setting CSS, Watch mode, etc.

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,25 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: {
-    homePage: "./src/index.js",
-    menuPage: "./src/menu.js",
-    contactPage: "./src/contact.js",
+    main: "./src/main.js",
+    home: "./src/home.js",
+    menu: "./src/menu.js",
+    contact: "./src/contact.js",
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  plugins: [new HtmlWebpackPlugin()],
+  devtool: "inline-source-map",
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   module: {
     rules: [
       {
