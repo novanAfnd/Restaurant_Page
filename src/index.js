77123c44fc8232bd1be7a7ff5a8bfd67b030ef3b
novanAfnd <novanafandi01@gmail.com>
2022-12-30 10:19:14 +0700

    Setting CSS and Fonts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import "./style.css";
 
-function component() {
+const home = document.createElement("div");
+home.className = "home";
+home.innerHTML = "Hello Webpack";
+
+document.body.appendChild(home);
+
+// if the manual form without the function as above
+//..cannot be used for switching to menus and contacts,
+//..then the functional form as below is worth a try
+/* function component() {
   const element = document.createElement("div");
-
   element.innerHTML = "Hello webpack";
-
   element.classList.add("hello");
-
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component()); */
