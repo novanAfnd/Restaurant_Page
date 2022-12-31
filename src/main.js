b44899f790aa824f51d89a6433e54c58c4e8c105
Novan Afandi <novanafandi01@gmail.com>
2022-12-31 07:35:36 +0700

    Cleaning dist folder

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import "./style.css";
 
-const main = document.createElement("div");
-main.className = "main";
-main.innerHTML = "Hello Webpack";
+// Header
 
-document.body.appendChild(main);
+// Content for home, menu and contact
+const content = document.createElement("div");
+content.className = "content";
+content.innerHTML = "This is for content";
 
-// if the manual form without the function as above
-//..cannot be used for switching to menus and contacts,
-//..then the functional form as below is worth a try
-/* function component() {
-  const element = document.createElement("div");
-  element.innerHTML = "Hello webpack";
-  element.classList.add("hello");
-  return element;
-}
+document.body.appendChild(content);
 
-document.body.appendChild(component()); */
+// Footer
