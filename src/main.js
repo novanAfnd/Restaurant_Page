f40a09bb439f928d06f7ab331d54bbd21e81d0a4
Novan Afandi <novanafandi01@gmail.com>
2023-01-01 10:08:14 +0700

    Setting CSS Position 2

--- src/main.js
+++ src/main.js
@@ -13,11 +13,6 @@ content.innerHTML =
 
 document.body.appendChild(content);
 
-const content2 = document.createElement("div");
-content2.className = "content2";
-content2.innerHTML =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosamLorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam..";
-
 // Footer
 const footer = document.createElement("footer");
 footer.className = "footer";
