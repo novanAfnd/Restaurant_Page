1cac35b7ac4387a433f6f740adc56d11ba7fcc5f
Novan Afandi <novanafandi01@gmail.com>
2023-01-01 10:05:08 +0700

    Setting CSS Position

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import "./style.css";
 
 // Header
+const header = document.createElement("header");
+header.className = "header";
+document.body.appendChild(header);
 
 // Content for home, menu and contact
 const content = document.createElement("div");
 content.className = "content";
-content.innerHTML = "This is for content";
+content.innerHTML =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosamLorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam..";
 
 document.body.appendChild(content);
 
+const content2 = document.createElement("div");
+content2.className = "content2";
+content2.innerHTML =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosamLorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam..";
+
 // Footer
+const footer = document.createElement("footer");
+footer.className = "footer";
+document.body.appendChild(footer);
