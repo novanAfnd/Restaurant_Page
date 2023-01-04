e5de34a1f38d6fc6f316b4ffca6a992a5ebce8d6
Novan Afandi <novanafandi01@gmail.com>
2023-01-04 21:38:46 +0700

    Fix header and footer list

---
+++ src/home.js
@@ -0,0 +1,15 @@
+homeContent = document.querySelector(".content");
+homeContent.textContent = "lorem";
+
+// konsep button active dan tidak active
+// saat button untuk home aktif, maka button untuk menu dan contact tidak aktif
+//...begitu pula untuk button menu dan contact
+//...sehingga nanti mungkin pakai loop untuk add active atau remove active
+
+// kita buat kode agar masing-masing button bekerja semestinya dulu
+//...kemudian kita isi kontennya oke?
+
+// kita edit isi dari home page disini
+//...lalu isi dari home page tersebut kita jadikan function
+//...kemudian function tersebut kita export dan diimport di main.js
+//...di dalam main.js kita buat button untuk memanggil function disini
--- src/main.js
+++ src/main.js
@@ -1,56 +1,100 @@
+// import
 import "./style.css";
 
-// Header
+// Header Header Header Header Header Header Header Header Header Header Header
 const mainHeader = document.createElement("header");
 mainHeader.className = "header";
-document.body.appendChild(mainHeader);
-
 const headerNav = document.createElement("nav");
+const headerList = document.createElement("ul");
+headerList.className = "header_list";
+
+document.body.appendChild(mainHeader);
 mainHeader.appendChild(headerNav);
+headerNav.appendChild(headerList);
 
-const tabList = document.createElement("ul");
-tabList.className = "header_tab-list";
-headerNav.appendChild(tabList);
+/* Function to create list with class, id and text */
+function createHeaderList(liClass, liId, liText) {
+  const li = document.createElement("li");
+  li.className = liClass;
+  li.id = liId;
+  li.textContent = liText;
+  return li;
+}
 
-const homeLi = document.createElement("li");
-homeLi.id = "home";
-homeLi.textContent = "Home";
-const menuLi = document.createElement("li");
-menuLi.id = "menu";
-menuLi.textContent = "Menu";
-const contactLi = document.createElement("li");
-contactLi.id = "contact";
-contactLi.textContent = "Cotact";
-tabList.appendChild(homeLi);
-tabList.appendChild(menuLi);
-tabList.appendChild(contactLi);
+/* Function to add list to header or footer ul */
+function appendChildren(parent, children) {
+  children.forEach(function (child) {
+    parent.appendChild(child);
+  });
+}
 
-// Content for home, menu and contact
+const HeaderListItems = [
+  createHeaderList("header_list_item", "home", "Home"),
+  createHeaderList("header_list_item", "menu", "Menu"),
+  createHeaderList("header_list_item", "contact", "Contact"),
+];
+
+appendChildren(headerList, HeaderListItems);
+
+/* Function for each header list */
+document.getElementById("home").addEventListener("click", function () {
+  alert("Home");
+});
+
+document.getElementById("menu").addEventListener("click", function () {
+  alert("Menu");
+});
+
+document.getElementById("contact").addEventListener("click", function () {
+  alert("Contact");
+});
+
+// Content Content Content Content Content Content Content Content for home, menu and contact
 const content = document.createElement("div");
 content.className = "content";
-content.innerHTML =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosamLorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis. Eligendi quia dolorem totam quis autem laboriosam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque unde numquam expedita, molestiae pariatur odit quaerat omnis cupiditate dignissimos a, dolorum recusandae nobis.";
-
 document.body.appendChild(content);
 
-// Footer
+// Footer Footer Footer Footer Footer Footer Footer Footer Footer Footer Footer Footer
 const mainFooter = document.createElement("footer");
 mainFooter.className = "footer";
-document.body.appendChild(mainFooter);
 
 const footerList = document.createElement("ul");
 footerList.className = "footer_list";
+
+document.body.appendChild(mainFooter);
 mainFooter.appendChild(footerList);
 
-const creditOne = document.createElement("li");
-creditOne.id = "credit-one";
-creditOne.textContent = "bla bla made by ...";
-const creditTwo = document.createElement("li");
-creditTwo.id = "credit-two";
-creditTwo.textContent = "bla bla made by ...";
-const creditThree = document.createElement("li");
-creditThree.id = "credit-three";
-creditThree.textContent = "bla bla made by ...";
-footerList.appendChild(creditOne);
-footerList.appendChild(creditTwo);
-footerList.appendChild(creditThree);
+/* Create list containing anchor(with class, href and text) */
+function createAnchorInsideList(aClass, aHref, aText) {
+  const li = document.createElement("li");
+  const anchor = document.createElement("a");
+  anchor.className = aClass;
+  anchor.href = aHref;
+  anchor.textContent = aText;
+  li.appendChild(anchor);
+  return li;
+}
+
+const footerListItems = [
+  createAnchorInsideList(
+    "footer_list_item",
+    "https://befonts.com/regattia-typeface.html",
+    "Regattia Typeface font designed by Bruno Sáez López - befonts.com"
+  ),
+  createAnchorInsideList(
+    "footer_list_item",
+    "https://fonts.google.com/specimen/Roboto",
+    "Roboto font designed by Christian Robertson - fonts.google.com"
+  ),
+  createAnchorInsideList(
+    "footer_list_item",
+    "https://dribbble.com/shots/17319830-Stadshaven-Brewery-Restaurant-page",
+    "Design inspired by Gil - dribbble.com"
+  ),
+];
+
+/* Call appendChildren above */
+appendChildren(footerList, footerListItems);
+
+console.log(footerListItems);
+console.log(footerList);
