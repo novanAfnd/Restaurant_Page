fc4e1044f8458fe2d6d5068f5786b195f3a4ed07
Novan Afandi <novanafandi01@gmail.com>
2023-01-11 22:08:33 +0700

    Fix menu

--- src/menu.js
+++ src/menu.js
@@ -4,12 +4,38 @@ menuContentContainer.className = "menu_content_container";
 const menuContentInner = document.createElement("div");
 menuContentInner.className = "content_container_inner";
 
+// Header
 const menuHeader = document.createElement("div");
 menuHeader.className = "content_header";
 menuHeader.textContent = "Menu";
+
+// Submenu Container (Container for each food types)
+const subMenuFoods = document.createElement("div");
+subMenuFoods.className = "menu_content_submenu_container";
+subMenuFoods.id = "submenu_foods";
+const subMenuDrinks = document.createElement("div");
+subMenuDrinks.className = "menu_content_submenu_container";
+subMenuDrinks.id = "submenu_drinks";
+
+// Submenu Header
+const subMenuFoodsHeader = document.createElement("h3");
+subMenuFoodsHeader.textContent = "Foods";
+subMenuFoods.appendChild(subMenuFoodsHeader);
+const subMenuDrinksHeader = document.createElement("h3");
+subMenuDrinksHeader.textContent = "Drinks";
+subMenuDrinks.appendChild(subMenuDrinksHeader);
+
+// Foods
+
+// Drinks
 
 export function menuActive() {
   content.appendChild(menuContentContainer);
   menuContentContainer.appendChild(menuContentInner);
   menuContentInner.appendChild(menuHeader);
+  // Foods
+  menuContentInner.appendChild(subMenuFoods);
+
+  // Drinks
+  menuContentInner.appendChild(subMenuDrinks);
 }
